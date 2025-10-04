a0757c6f1f93ce903b9c30eb95077d4c34385b5f
Masaxx <64898857+Masaxx@users.noreply.github.com>
2025-10-04 15:42:00 +0200

    Update auth.ts

--- backend/middleware/middleware/auth.ts
+++ backend/middleware/middleware/auth.ts
@@ -11,6 +11,10 @@ if (!process.env.JWT_SECRET && process.env.NODE_ENV === 'production') {
 
 export interface AuthRequest extends Request {
   user?: User;
+  body: any;
+  params: any;
+  query: any;
+  headers: any;
   files?: any;
 }
 
