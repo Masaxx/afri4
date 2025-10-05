40b1731a0b19fa799d48aec94e8a9ba36608e84d
Masaxx <64898857+Masaxx@users.noreply.github.com>
2025-10-05 15:07:20 +0200

    Update drizzle.config.js

--- backend/drizzle.config.js
+++ backend/drizzle.config.js
@@ -1,8 +1,8 @@
 import { defineConfig } from "drizzle-kit";
 
 export default defineConfig({
-  schema: "./src/shared/schema.ts", // adjust path if needed
-  out: "./src/db/migrations",
+  schema: "./shared/schema.ts", // adjust path if needed
+  out: "./drizzle",
   driver: "pg",
   dialect: "postgresql",
   dbCredentials: {
